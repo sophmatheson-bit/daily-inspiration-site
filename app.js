6f2f2220186e8819123b7cfffc544fedbe113d86
sophmatheson-bit <sophiamatheson88@gmail.com>
2025-12-19 21:51:28 -0500

    Update app.js
    Testing additional photos

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const quotes = [
 const photos = [
   "/photos/1.jpeg",
   "/photos/2.jpeg",
-  "/photos/3.jpeg"
+  "/photos/3.jpeg",
+  "/photos/4.jpeg",
+  "/photos/5.jpeg",
+  "/photos/6.jpeg",
+  "/photos/7.jpeg",
+  "/photos/8.jpeg",
+  "/photos/9.jpeg",
+  "/photos/10.jpeg",
+  "/photos/12.jpeg",
+  "/photos/13.jpeg",
+  "/photos/14.jpeg",
+  "/photos/15.jpeg",
+  "/photos/16.jpeg"
 ];
 
 // Pick random quote and photo AFTER defining the arrays
